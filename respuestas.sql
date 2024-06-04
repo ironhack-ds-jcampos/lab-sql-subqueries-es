0df6e82453991c930ce86c8dc4e407714c5aef41
TheMineWay <joelcamposoliva9000@gmail.com>
2024-06-04 20:28:10 +0200

    5

--- respuestas.sql
+++ respuestas.sql
@@ -24,4 +24,19 @@ INNER JOIN (
 ) f ON f.film_id = fa.film_id;
 
 /* 4 */
+SELECT f.*
+FROM film f
+INNER JOIN film_category fc ON fc.film_id = f.film_id
+INNER JOIN category c ON c.category_id = fc.category_id
+WHERE c.name = 'Family';
+
 /* 5 */
+SELECT c.first_name, c.email
+FROM customer c
+WHERE c.address_id IN (
+	SELECT a.address_id
+	FROM address a
+	INNER JOIN city c ON c.city_id = a.city_id
+	INNER JOIN country c2 ON c2.country_id = c.country_id
+	WHERE c2.country = 'Canada'
+);
